5a45cff539336a236981e7f58cdb744907b6c6af
Matheus Gomes <gomes.andrade@academico.ifpb.edu.br>
2021-11-07 17:05:17 -0300

    adicionado questao 12

---
+++ questao12.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo '
+$PWD (valor atual: /home/matheus/Lista1-script) variável que guarda o valor do diretório atual.
+
+$OLDPWD (valor atual: /home/matheus) variável que guarda o valor do diretório anterior ao atual.
+
+$LANG (valor  atual: pt_BR.UTF-8) variável que mostra a linguagem preferida do usuário.
+
+$SSH_CONNECTION (valor atual: 192.168.1.105 49791 192.168.1.106 22) mostra o endereço do cliente, a porta de saída no cliente, o endereço do servidor e a porta de entrada no servidor.
+
+$TERM (valor atual: xterm-256color) mostra o tipo de terminal virtual que está sendo usado.
+
+$SHLVL (valor atual: 1) mostra o número de shells atualmente em execução.
+
+$LOGNAME (valor atual: matheus) mostra o nome de acesso do usuário.
+
+$PATH (valor atual: /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin) mostra o caminho de busca dos comandos digitados pelo usuário.'
